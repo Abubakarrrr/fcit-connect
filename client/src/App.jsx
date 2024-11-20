ea0cf5cc3d5535eeb7e93f7c5ca193a60da706b0
Abubakar <abubakkeramjad10@gmail.com>
2024-11-20 14:20:21 +0500

    enabled analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
+import "./App.css";
+import Home from "./pages/Home";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "./components/shared/Navbar";
 
 function App() {
-
   return (
-   <h1>ok</h1>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Analytics/>
+      <Navbar />
+      <Router>
+        <div className="font-primary">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/blog" element={<Blog />} /> 
+            <Route path="/about" element={<About />} /> 
+            <Route path="/contact" element={<Contact />} />
+             <Route path="/services" element={<Services />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/shared/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ModeToggle } from '../mode-toggle'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>navbar</h1>
+        <ModeToggle/>
+    </div>
+  )
+}
+
+export default Navbar
