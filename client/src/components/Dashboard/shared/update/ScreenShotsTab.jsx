2f271bc66d89fb6773a230dd69cff1b9d0318ce4
Umair Sarwar <umairsarwar4254@gmail.com>
2025-01-26 20:26:01 +0500

    screenshots upload handled

--- client/src/components/Dashboard/shared/update/ScreenShotsTab.jsx
+++ client/src/components/Dashboard/shared/update/ScreenShotsTab.jsx
@@ -7,26 +7,13 @@ import { useProjectStore } from "@/store/projectStore";
 
 const ScreenShotsTab = ({ images, setImages }) => {
   console.log(images);
-  return (
-    <div className="py-6 px-4 rounded-lg border shadow-sm">
-      <h1 className="text-lg font-semibold">Screenshots</h1>
-      <div>
-        <ImageUploader images={images} setImages={setImages} />
-      </div>
-    </div>
-  );
-};
-
-export default ScreenShotsTab;
-
-function ImageUploader({ images, setImages }) {
   const { deleteFile, uploadFile, project } = useProjectStore();
   const handleRemoveImage = async (index) => {
     try {
-      // setImages(images.filter((_, i) => i !== index));
       await deleteFile(images[index]);
+      setImages(images.filter((_, i) => i !== index));
     } catch (error) {
-      console.log(url);
+      console.log(error);
     }
   };
 
@@ -36,89 +23,82 @@ function ImageUploader({ images, setImages }) {
       alert("You can upload a maximum of 3 images.");
       return;
     }
-    // for (const file of files) {
-    //   try {
-    //     const url = await uploadFile(
-    //       file,
-    //       `screenshot${images.length + 1}`,
-    //       project?._id
-    //     );
-    //     setImages((prevImages) => [...prevImages, url]);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
+    for (const file of files) {
+      try {
+        const url = await uploadFile(
+          file,
+          `screenshot${images.length + 1}`,
+          project?._id
+        );
+        setImages((prevImages) => [...prevImages, url]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
-
   return (
-    <div>
-      <div className="space-y-4">
-        <input
-          type="file"
-          accept="image/*"
-          multiple
-          onChange={handleImageUpload}
-          className="hidden"
-          id="image-upload"
-        />
+    <div className="py-6 px-4 rounded-lg border shadow-sm">
+      <h1 className="text-lg font-semibold">Screenshots</h1>
+      <div>
+        <div className="space-y-4">
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={handleImageUpload}
+            className="hidden"
+            id="image-upload"
+          />
 
-        <div className="grid grid-cols-3 gap-4">
-          {images.length > 0 ? (
-            images.map((image, index) => (
-              <div
-                key={index}
-                className="relative aspect-[3/2] bg-gray-100 rounded-lg overflow-hidden"
-              >
-                <img
-                  src={image}
-                  alt={`Uploaded ${index + 1}`}
-                  className="w-full h-full object-cover"
-                />
-                {index + 1 == images.length && (
-                  <button
-                    onClick={() => handleRemoveImage(index)}
-                    className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1 hover:bg-red-600"
-                    aria-label={`Remove image ${index + 1}`}
-                  >
-                    <X size={16} />
-                  </button>
-                )}
+          <div className="grid grid-cols-3 gap-4">
+            {images.length > 0 ? (
+              images.map((image, index) => (
+                <div
+                  key={index}
+                  className="relative aspect-[3/2] bg-gray-100 rounded-lg overflow-hidden"
+                >
+                  <img
+                    src={image}
+                    alt={`Uploaded ${index + 1}`}
+                    className="w-full h-full object-cover"
+                  />
+                  {index + 1 == images.length && (
+                    <button
+                      onClick={() => handleRemoveImage(index)}
+                      className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1 hover:bg-red-600"
+                      aria-label={`Remove image ${index + 1}`}
+                    >
+                      <X size={16} />
+                    </button>
+                  )}
 
-                <div className="absolute bottom-2 left-2 bg-black/50 text-white text-xs px-2 py-1 rounded">
-                  {index + 1}/3
+                  <div className="absolute bottom-2 left-2 bg-black/50 text-white text-xs px-2 py-1 rounded">
+                    {index + 1}/3
+                  </div>
                 </div>
+              ))
+            ) : (
+              <div className="w-full h-[100px] flex items-center justify-center text-gray-400 col-span-3">
+                No image selected
               </div>
-            ))
-          ) : (
-            <div className="w-full h-[100px] flex items-center justify-center text-gray-400 col-span-3">
-              No image selected
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col items-center justify-center py-2">
-          <label
-            htmlFor="image-upload"
-            className="cursor-pointer text-[14px] px-4 py-2 bg-black text-white rounded-lg"
-          >
-            Choose File(s)
-          </label>
-          <p className="text-[12px] mt-1 text-gray-500">
-            {" "}
-            Images upto 1 MB,max 3
-          </p>
+            )}
+          </div>
+          <div className="flex flex-col items-center justify-center py-2">
+            <label
+              htmlFor="image-upload"
+              className="cursor-pointer text-[14px] px-4 py-2 bg-black text-white rounded-lg"
+            >
+              Choose File(s)
+            </label>
+            <p className="text-[12px] mt-1 text-gray-500">
+              {" "}
+              Images upto 1 MB,max 3
+            </p>
+          </div>
         </div>
       </div>
-      {/* <div className="flex justify-end mt-6">
-        <Button variant="" className="aspect-square max-sm:p-0">
-          <PlusCircle
-            className=" sm:-ms-1 sm:me-2"
-            size={16}
-            strokeWidth={2}
-            aria-hidden="true"
-          />
-          <span className="max-sm:sr-only">Save Changes</span>
-        </Button>
-      </div> */}
     </div>
   );
-}
+};
+
+export default ScreenShotsTab;
