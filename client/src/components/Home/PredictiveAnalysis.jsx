25ce0b9ae5dc003cdc26c56d259bb3738bef21de
Abubakar <abubakkeramjad10@gmail.com>
2025-05-19 23:04:27 +0500

    video upload

--- client/src/components/Home/PredictiveAnalysis.jsx
+++ client/src/components/Home/PredictiveAnalysis.jsx
@@ -5,15 +5,15 @@ export function HeroVideoDialogDemo() {
       <HeroVideoDialog
         className="dark:hidden block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+        videoSrc="/predictive.mp4"
+        thumbnailSrc="/predictive.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        videoSrc="/predictive.mp4"
+        thumbnailSrc="/predictive.png"
         thumbnailAlt="Hero Video"
       />
     </div>
